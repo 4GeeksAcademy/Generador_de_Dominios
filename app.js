8cfb87e7ad87181810476b5c7a81b83ccd8d0427
MariaVictoriaZ <163335173+MariaVictoriaZ@users.noreply.github.com>
2024-04-29 19:28:40 +0000

    Second commit

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
-let pronoun = ['the', 'our'];
-let adj = ['amazing', 'little'];
-let noun = ['dog', 'guy'];
+let pronouns = ['the', 'our'];
+let adjs = ['amazing', 'little'];
+let nouns = ['dog', 'guy'];
+let extensions = ['net', 'com', 'io', 'us'];
 
 let nombresDominios = [];
 
-for (let i = 0; i < pronoun.length; i++) {
-  for (let j = 0; j < adj.length; j++) {
-      for (let k = 0; k < noun.length; k++) {
-          let Formula = pronoun[i] + adj[j] + noun[k] + '.com';
-          nombresDominios.push(Formula);
-      }
-  }
-}
+// for (let i = 0; i < pronouns.length; i++) {
+//   for (let j = 0; j < adjs.length; j++) {
+//       for (let k = 0; k < nouns.length; k++) {
+//         nombresDominios.push(pronouns[i] + adjs[j] + nouns[k] + '.com');
+//       }
+//   }
+// }
 
-for (let f = 0; f < nombresDominios.length; f++) {
-  console.log(nombresDominios[f]);
-}
-      
+// for (let f = 0; f < nombresDominios.length; f++) {
+//   console.log(nombresDominios[f]);
+// }
+
+
+
+
+
+pronouns.forEach(pronoun =>{
+  adjs.forEach(adj =>{
+      nouns.forEach(noun =>{
+          extensions.forEach(extension =>{
+              nombresDominios.push(pronoun + adj + noun + extension);
+          });
+      });
+  });
+});
+
+console.log(nombresDominios);
+   
